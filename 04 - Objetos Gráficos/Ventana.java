ef4fb0a439699d8992271a7f2f930c17d11debc4
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-03-05 15:58:48 -0600

    Sesión 19 - 05/03/2025

--- 04 - Objetos Gráficos/Ventana.java
+++ 04 - Objetos Gráficos/Ventana.java
@@ -2,13 +2,8 @@ import java.awt.*;
 
 public class Ventana extends Frame {
 
-    private Dibujo dibujo;
-
     public Ventana() {
         super("Objetos Gráficos");
-
-        dibujo = new Dibujo();
-        add(dibujo,"Center");
     }
 
     public boolean handleEvent(Event e) {
@@ -18,6 +13,22 @@ public class Ventana extends Frame {
             return true;
         }
         return super.handleEvent(e);
+    }
+
+    public void paint(Graphics g) {
+        Font f = new Font("Arial", Font.BOLD + Font.ITALIC, 16);
+        Color c = new Color(0x4D, 0x55, 0xCC);
+        g.setColor(Color.RED);
+        g.setFont(f);
+        g.drawString("Saludos Programas!!!!", 100, 100);
+
+        g.drawLine(150, 50, 100, 90);
+        g.fillRect(155, 200, 90, 70);
+        g.fillRoundRect(150, 99, 100, 80, 25, 50);
+
+        g.setColor(Color.blue);
+        g.fillOval(170, 99, 39, 99);
+        g.drawOval(450, 50, 130, 130);
     }
     
     public static void main(String[] args) {
