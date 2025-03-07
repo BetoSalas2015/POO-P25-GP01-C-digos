60f659455e02d9479e67aed1e48de7d7738df791
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-03-07 15:42:31 -0600

    Sesión 20 - 07/03/2025

--- 04 - Objetos Gráficos/Ventana.java
+++ 04 - Objetos Gráficos/Ventana.java
@@ -16,7 +16,10 @@ public class Ventana extends Frame {
     }
 
     public void paint(Graphics g) {
-        Font f = new Font("Arial", Font.BOLD + Font.ITALIC, 16);
+        Graphics2D g2d = (Graphics2D) g;
+
+
+        /*Font f = new Font("Arial", Font.BOLD + Font.ITALIC, 16);
         Color c = new Color(0x4D, 0x55, 0xCC);
         g.setColor(Color.RED);
         g.setFont(f);
@@ -29,6 +32,18 @@ public class Ventana extends Frame {
         g.setColor(Color.blue);
         g.fillOval(170, 99, 39, 99);
         g.drawOval(450, 50, 130, 130);
+
+        Color color = new Color(50, 26, 94);
+        g.setColor(Color.black);
+        g.fillRect(0,0,getWidth(), getHeight());
+        g.setColor(Color.yellow);
+        g.fillArc(70, 70, 175, 175, 35, 280);
+        g.setColor(color);
+        g.fillOval(140, 110, 18, 18);
+        g.setColor(new Color(232, 214, 19));
+        g.fillOval(200, 145, 30, 30); 
+        g.fillOval(270, 145, 30, 30);
+        g.fillOval(340, 145, 30, 30);*/
     }
     
     public static void main(String[] args) {
