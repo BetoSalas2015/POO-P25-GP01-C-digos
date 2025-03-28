a319461f2bb3c06caae0ca6297ea6a9e355f42bc
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2025-03-28 14:29:18 -0600

    Sesión 27 - 26/03/2025

--- 04 - Objetos Gráficos/Ventana.java
+++ 04 - Objetos Gráficos/Ventana.java
@@ -1,4 +1,5 @@
 import java.awt.*;
+import java.awt.geom.*;
 
 public class Ventana extends Frame {
 
@@ -16,7 +17,25 @@ public class Ventana extends Frame {
     }
 
     public void paint(Graphics g) {
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D gc2d = (Graphics2D) g; 
+	        Color color = new Color(50, 26, 94);
+	        gc2d.setColor(Color.BLACK);
+	        gc2d.setBackground(Color.black);
+	        gc2d.clearRect(0, 0, getWidth(), getHeight());
+	        gc2d.setStroke(new BasicStroke(4.0f));
+	        gc2d.setColor(Color.GRAY);
+	        gc2d.fill( new Arc2D.Float(68, 68, 170, 179, 35, 280, Arc2D.PIE) );
+	        gc2d.setPaint(new GradientPaint(145, 180, Color.YELLOW, 
+	                                        50, 50, Color.LIGHT_GRAY, true));
+	        gc2d.fill( new Arc2D.Float(70, 70, 175, 175, 35, 280, Arc2D.PIE) );
+	        gc2d.fill( new Arc2D.Float(70, 70, 175, 175, 35, 200, Arc2D.PIE) );
+	        gc2d.setColor(color);
+	        gc2d.fill(new Ellipse2D.Float(140,110,18,18));
+	        gc2d.setPaint(new GradientPaint(200, 100, new Color(232,214,19), 
+	                                        230, 160, Color.YELLOW, true));
+	        gc2d.fill(new Ellipse2D.Float(200, 145, 30, 30));
+	        gc2d.fill(new Ellipse2D.Float(270, 145, 30, 30));
+	        gc2d.fill(new Ellipse2D.Float(340, 145, 30, 30));
 
 
         /*Font f = new Font("Arial", Font.BOLD + Font.ITALIC, 16);
---
+++ 06 - Objetos de interfaz/Calculadora.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+
+public class Calculadora extends Frame {
+    private Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    private Button btnMas, btnMenos, btnMult, btnDiv, btnPunto, btnIgual, btnC;
+    private TextField txtDisplay;
+    private Panel pnlTeclado, pnlDisplay;
+
+    public Calculadora() {
+        super("Calculadora"); 
+
+        btn0 = new Button("0");         btn1 = new Button("1");
+        btn2 = new Button("2");         btn3 = new Button("3");
+        btn4 = new Button("4");         btn5 = new Button("5");
+        btn6 = new Button("6");         btn7 = new Button("7");
+        btn8 = new Button("8");         btn9 = new Button("9");
+        btnMas = new Button("+");       btnMenos = new Button("-");
+        btnMult = new Button("*");      btnDiv = new Button("/");
+        btnPunto = new Button(".");     btnIgual = new Button("=");
+        btnC = new Button("C");
+
+        txtDisplay = new TextField("0");
+
+        pnlDisplay = new Panel();
+        pnlTeclado = new Panel();
+
+        pnlTeclado.setLayout( new GridLayout(4, 4, 2, 2) );
+        pnlTeclado.add(btn7);
+        pnlTeclado.add(btn8);
+        pnlTeclado.add(btn9);
+        pnlTeclado.add(btnDiv);
+        pnlTeclado.add(btn4);
+        pnlTeclado.add(btn5);
+        pnlTeclado.add(btn6);
+        pnlTeclado.add(btnMult);
+        pnlTeclado.add(btn1);
+        pnlTeclado.add(btn2);
+        pnlTeclado.add(btn3);
+        pnlTeclado.add(btnMenos);
+        pnlTeclado.add(btnIgual);
+        pnlTeclado.add(btn0);
+        pnlTeclado.add(btnPunto);
+        pnlTeclado.add(btnMas);
+
+        pnlDisplay.setLayout(new BorderLayout() );
+        pnlDisplay.add(btnC, "East");
+        pnlDisplay.add(txtDisplay,"Center");
+
+        //  BorderLayout es default a Frame
+        add(pnlDisplay, "North");
+        add(pnlTeclado, "Center");
+    }
+
+
+    public static void main(String args[]) {
+        Calculadora calc = new Calculadora();
+        calc.resize(300, 320);
+        calc.show();
+    }
+}
